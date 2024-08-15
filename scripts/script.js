fc789efa1306d72949d6a86707fba8f6e8d4cb72
daniel <vega.enrique.martin@gmail.com>
2024-08-15 15:43:34 -0600

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+document.getElementById("contactButton").addEventListener("click", function() {
+    window.location.href = "mailto:vega.enrique.martin@gmail.com?subject=Contacto%20Profesional&body=Hola%20Daniel%20Ortega,%0A%0AEstoy%20interesado%20en%20contactarte%20por%20motivos%20profesionales.%0A%0A¡Gracias!";
+});
